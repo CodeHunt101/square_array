dec4115fb6fcd71c750211f3e35bca8df6355284
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-12 17:05:22 +1000

    Lab completed

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each{|n| squared_array<<(n**2)}
+  squared_array
 end
+
+def square_array_collect(array)
+  array.collect {|n| n**2}
+end 
+
+square_array_collect([1,2,3])
